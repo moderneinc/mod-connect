d9abd4d0444b766bf393e74da462c762b5a4240a
Tim te Beek <tim@moderne.io>
2023-11-21 14:16:38 +0100

    shortName is always present

--- src/main/java/io/moderne/connect/commands/Jenkins.java
+++ src/main/java/io/moderne/connect/commands/Jenkins.java
@@ -516,9 +516,7 @@ public class Jenkins implements Callable<Integer> {
         for (int i = 0; i < pluginsSize; i++) {
             JsonNode pluginNode = pluginsNode.get(i);
             if (pluginNode.get("active").asBoolean(false)) {
-                if (pluginNode.has("shortName")) {
-                    result.put(pluginNode.get("shortName").asText(), pluginNode.get("version").asText());
-                }
+                result.put(pluginNode.get("shortName").asText(), pluginNode.get("version").asText());
             }
         }
         if (!result.keySet().containsAll(requiredPlugins)) {
